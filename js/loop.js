91768bd7a58294909f3b71f30efedb8d4ade1993
mustakin04 <mustakinhasan37@gmail.com>
2024-09-17 01:09:49 +0600

    namata

---
+++ js/loop.js
@@ -0,0 +1,6 @@
+let num;
+for(num=10; num<15; num++){
+for(let i=1; i<11; i++){
+    document.write(num +"x"+ i+"=" +num*i+ "<br>");
+}
+}
---
+++ js/while.js
@@ -0,0 +1,5 @@
+let num=1;
+while(num<11){
+    document.write(num );
+    num++;
+}
